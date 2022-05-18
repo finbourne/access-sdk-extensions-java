965bf41f8055df02617bd71afce94a0401594304
Matthew Boardman <90201501+matthew-boardman@users.noreply.github.com>
2022-05-18 09:22:21 +0100

    CTECH-1303 test setup (#1)

--- sdk/src/test/integration/java/com/finbourne/access/extensions/ApiFactoryBuilderTests.java
+++ sdk/src/test/integration/java/com/finbourne/access/extensions/ApiFactoryBuilderTests.java
@@ -1,38 +1,38 @@
 package com.finbourne.access.extensions;
 
 import com.finbourne.access.ApiException;
-// UNCOMMENT BELOW TWO LINES TO IMPORT AN API AND THE TYPE A CALL FROM IT RETURNS
-// import com.finbourne.access.api.;
-// import com.finbourne.access.model.;
+import com.finbourne.access.api.ApplicationMetadataApi;
+import com.finbourne.access.model.ResourceListOfAccessControlledResource;
 import com.finbourne.access.extensions.auth.FinbourneTokenException;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.Matchers.empty;
 import static org.junit.Assert.assertThat;
 
-// UNCOMMENT BELOW TESTS AND USE IMPORTED API TO MAKE A VALID CALL - DRIVE FOLDERS API IS BEING USED AS AN EXAMPLE
-// public class ApiFactoryBuilderTests {
+ public class ApiFactoryBuilderTests {
 
-//     @Rule
-//     public ExpectedException thrown = ExpectedException.none();
+     @Rule
+     public ExpectedException thrown = ExpectedException.none();
 
-//     @Test
-//     public void build_WithExistingConfigurationFile_ShouldReturnFactory() throws ApiException, ApiConfigurationException, FinbourneTokenException {
-//         ApiFactory apiFactory = ApiFactoryBuilder.build(CredentialsSource.credentialsFile);
-//         assertThat(apiFactory, is(notNullValue()));
-//         assertThatFactoryBuiltApiCanMakeApiCalls(apiFactory);
-//     }
+     @Test
+     public void build_WithExistingConfigurationFile_ShouldReturnFactory() throws ApiException, ApiConfigurationException, FinbourneTokenException {
+         ApiFactory apiFactory = ApiFactoryBuilder.build(CredentialsSource.credentialsFile);
+         assertThat(apiFactory, is(notNullValue()));
+         assertThatFactoryBuiltApiCanMakeApiCalls(apiFactory);
+     }
 
-//     private static void assertThatFactoryBuiltApiCanMakeApiCalls(ApiFactory apiFactory) throws ApiException {
-//         FoldersApi foldersApi = apiFactory.build(FoldersApi.class);
-//         PagedResourceListOfStorageObject rootFolder = foldersApi.getRootFolder(null, null, null, null, null);
-//         assertThat("Folders API created by factory should return root folder"
-//                 , rootFolder, is(notNullValue()));
-//         assertThat("Root folder contents types returned by the folders API should not be empty",
-//                 rootFolder.getValues(), not(empty()));
-//     }
+     private static void assertThatFactoryBuiltApiCanMakeApiCalls(ApiFactory apiFactory) throws ApiException {
+         ApplicationMetadataApi applicationMetadataApi = apiFactory.build(ApplicationMetadataApi.class);
+         ResourceListOfAccessControlledResource listOfAccessControlledResource = applicationMetadataApi.listAccessControlledResources();
+         assertThat("app metadata API created by factory should return list of resources"
+                 , listOfAccessControlledResource, is(notNullValue()));
+         assertThat("resource list contents types returned by the app metadata API should not be empty",
+                 listOfAccessControlledResource.getValues(), not(empty()));
+     }
 
-// }
+ }
